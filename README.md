638fda161eb0bed9e0fa0465380dcfbc8eab13f8
leonardwei2023 <57123326+leonardwei2023@users.noreply.github.com>
2022-12-15 02:29:22 -0800

    idk what im doing with tfmicro

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # fall-proj-2023
 
+### Setup
+1. Install Docker
+2. Clone tensorflow and tensor-lite micro repositories into ./src folder
+3. Download data and extract motion and sleep data to ./data folder
+
 ### Opening Juypter Notebook
 Run once to build docker container
 ```
@@ -10,3 +15,8 @@ Run to startup docker container
 docker run -itp 8888:8888 -v "/$(pwd)":/home/jovyan/work project:latest
 ```
 Then open link generated in terminal
+
+### Tensorflow Model
+Running
+
+### Buidling 
--- src/main-ble.cpp
+++ src/main-ble.cpp
@@ -5,6 +5,31 @@
 #include <BLE2902.h>
 #include <Arduino.h>
 
+#include "tflite-micro/lite/micro/all_ops_resolver.h"
+#include "tflite-micro/lite/micro/micro_error_reporter.h"
+#include "tflite-micro/lite/micro/micro_interpreter.h"
+#include "tflite-micro/lite/schema/schema_generated.h"
+#include "tflite-micro/lite/version.h"
+
+// Tensorflow
+// global variables used for TensorFlow Lite (Micro)
+tflite::MicroErrorReporter tflErrorReporter;
+// pull in all the TFLM ops, you can remove this line and
+// only pull in the TFLM ops you need, if would like to reduce
+// the compiled size of the sketch.
+tflite::AllOpsResolver tflOpsResolver;
+
+const tflite::Model* tflModel = nullptr;
+tflite::MicroInterpreter* tflInterpreter = nullptr;
+TfLiteTensor* tflInputTensor = nullptr;
+TfLiteTensor* tflOutputTensor = nullptr;
+
+// Create a static memory buffer for TFLM, the size may need to
+// be adjusted based on the model you are using
+constexpr int tensorArenaSize = 8 * 1024;
+byte tensorArena[tensorArenaSize] __attribute__((aligned(16)));
+
+// BLuetooth
 BLEServer* pServer = NULL;
 BLECharacteristic* pCharacteristic = NULL;
 bool deviceConnected = false;
@@ -14,7 +39,7 @@ bool twitched = false;
 #define SERVICE_UUID        "deadbeef-beef-beef-beef-deadbeefbeef"
 #define CHARACTERISTIC_UUID "beb5483e-36e1-4688-b7f5-ea07361b26a8"
 
-
+// Accel
 const int x_pin = 13;
 const int y_pin = 14;
 const int z_pin = 15;
@@ -27,6 +52,99 @@ float x_accel, y_accel, z_accel;
 void read_accel();
 void scale_accel();
 void print_accel();
+
+void setup() {
+  Serial.begin(115200);
+
+  /* TF setup */
+  // get the TFL representation of the model byte array
+  tflModel = tflite::GetModel(model);
+  if (tflModel->version() != TFLITE_SCHEMA_VERSION) {
+    Serial.println("Model schema mismatch!");
+    while (1);
+  // Create an interpreter to run the model
+  tflInterpreter = new tflite::MicroInterpreter(tflModel, tflOpsResolver, tensorArena, tensorArenaSize, &tflErrorReporter);
+  // Allocate memory for the model's input and output tensors
+  tflInterpreter->AllocateTensors();
+  // Get pointers for the model's input and output tensors
+  tflInputTensor = tflInterpreter->input(0);
+  tflOutputTensor = tflInterpreter->output(0);
+}
+
+  /* Bluetooth Setup */
+  BLEDevice::init("TwitchDetect");
+
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new ConnectionCallback());
+
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  pCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_UUID,
+                      BLECharacteristic::PROPERTY_READ   |
+                      BLECharacteristic::PROPERTY_WRITE  |
+                      BLECharacteristic::PROPERTY_NOTIFY |
+                      BLECharacteristic::PROPERTY_INDICATE
+                    );
+  pCharacteristic->setValue("TwitchDetect Init");
+  pCharacteristic->addDescriptor(new BLE2902());
+  pService->start();
+
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(true);
+  pAdvertising->setMinPreferred(0x06);
+  pAdvertising->setMinPreferred(0x12);
+
+  BLEDevice::startAdvertising();
+  Serial.println("Waiting a client connection to notify...");
+  
+  /* Setup the accelerometer */
+  x_sen = 0.3;
+  y_sen = 0.3;
+  z_sen = 0.3;
+
+  x_bias = 1.5;
+  y_bias = 1.5;
+  z_bias = 1.5;
+}
+
+void loop() {
+    // Sample accel and run inference
+    read_accel();
+    scale_accel();
+    tflInputTensor->data.f[0] = x_accel;
+    tflInputTensor->data.f[1] = y_accel;
+    tflInputTensor->data.f[2] = z_accel;
+    TfLiteStatus invokeStatus = tflInterpreter->Invoke();
+      if (invokeStatus != kTfLiteOk) {
+        Serial.println("Invoke failed!");
+        while (1);
+        return;
+      }
+    int value = tflOutputTensor.f[0];
+    
+    if (deviceConnected) {
+        Serial.printf("Sending value: %d\n", value);
+        pCharacteristic->setValue((uint8_t*)&value, 4);
+        pCharacteristic->notify();
+        delay(3);
+    }
+
+    if (!deviceConnected && oldDeviceConnected) {
+        delay(500); // give the bluetooth stack the chance to get things ready
+        pServer->startAdvertising(); // restart advertising
+        oldDeviceConnected = deviceConnected;
+    }
+
+    if (deviceConnected && !oldDeviceConnected) {
+        // do stuff here on connecting
+        oldDeviceConnected = deviceConnected;
+    }
+
+    
+    delay(100);
+}
 
 void read_accel() {
   x_raw = (float) analogRead(x_pin) / (float)1023;
@@ -54,73 +172,3 @@ class ConnectionCallback: public BLEServerCallbacks {
       deviceConnected = false;
     }
 };
-
-void setup() {
-  Serial.begin(115200);
-  
-  /* Setup the accelerometer */
-  
-  x_sen = 0.3;
-  y_sen = 0.3;
-  z_sen = 0.3;
-
-  x_bias = 1.5;
-  y_bias = 1.5;
-  z_bias = 1.5;
-
-  BLEDevice::init("TwitchDetect");
-
-  pServer = BLEDevice::createServer();
-  pServer->setCallbacks(new ConnectionCallback());
-
-  BLEService *pService = pServer->createService(SERVICE_UUID);
-
-  pCharacteristic = pService->createCharacteristic(
-                      CHARACTERISTIC_UUID,
-                      BLECharacteristic::PROPERTY_READ   |
-                      BLECharacteristic::PROPERTY_WRITE  |
-                      BLECharacteristic::PROPERTY_NOTIFY |
-                      BLECharacteristic::PROPERTY_INDICATE
-                    );
-  pCharacteristic->setValue("TwitchDetect Init");
-  pCharacteristic->addDescriptor(new BLE2902());
-  pService->start();
-
-  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-  pAdvertising->addServiceUUID(SERVICE_UUID);
-  pAdvertising->setScanResponse(true);
-  pAdvertising->setMinPreferred(0x06);
-  pAdvertising->setMinPreferred(0x12);
-
-  BLEDevice::startAdvertising();
-  Serial.println("Waiting a client connection to notify...");
-}
-
-void loop() {
-    int value = 1;
-
-    if (deviceConnected /*&& twitched*/) {
-        Serial.printf("Sending value: %d\n", 1);
-        pCharacteristic->setValue((uint8_t*)&value, 4);
-        // pCharacteristic->setValue(1);
-        pCharacteristic->notify();
-        // value++;
-        delay(3);
-    }
-
-    if (!deviceConnected && oldDeviceConnected) {
-        delay(500); // give the bluetooth stack the chance to get things ready
-        pServer->startAdvertising(); // restart advertising
-        oldDeviceConnected = deviceConnected;
-    }
-
-    if (deviceConnected && !oldDeviceConnected) {
-        // do stuff here on connecting
-        oldDeviceConnected = deviceConnected;
-    }
-
-    read_accel();
-    scale_accel();
-    //print_accel();
-    delay(100);
-}
